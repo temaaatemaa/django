6a575ab1703c127aae1999e8f5c1ecee7e9ab009
Artem <temka11@gmail.com>
2018-02-23 11:30:55 +0300

    table in setupuser

--- project/logic/urls.py
+++ project/logic/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.mainView, name='main'),
     url(r'^setup_user/(?P<pk>.+)/$',views.setupUser,name='setupUser'),
     url(r'^add_row/(?P<fio>.+)/(?P<system>.+)/(?P<priority>.+)/(?P<time>.+)/$',views.addRow,name='addRow'),
-    url(r'^del_row/(?P<fio>.+)/(?P<system>.+)/(?P<priority>.+)/(?P<time>.+)/$',views.delRow,name='delRow')
+    url(r'^del_row/(?P<fio>.+)/(?P<system>.+)/(?P<priority>.+)/(?P<time>.+)/$',views.delRow,name='delRow'),
+    url(r'^add_situation/$',views.delRow,name='addSituation')
 ]
